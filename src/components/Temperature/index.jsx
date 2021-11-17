71cca3b6a7fedf90817c704f3f24303953c28202
Joao Marcelo Alves <joao.alves@liferay.com>
2021-11-17 12:12:40 -0300

    Added Temperature

--- src/components/Temperature/index.jsx
+++ src/components/Temperature/index.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react"
+import { Form, Card } from "react-bootstrap";
+import { GiChickenOven, GiChicken, GiFrozenOrb } from "react-icons/gi";
 
-export default function Temperature({temp}) {
+import styles from "./Temperature.module.css";
+
+const iconSize = 40;
+
+
+
+export default function Temperature({ temp, onChange }) {
+
+  const tempStyle = temp >= 45 ? "hot" : temp === 0 ? "cold" : "safe";
   return (
-    <div>
-      <h1>Temperature</h1>
-      <span>{temp}°C</span>
-    </div>
-  )
+    <Card className={styles.container}>
+      <Card.Body>
+        <Card.Title className={styles.title}>Temperature
+        
+          {temp >= 45 ? (
+            <GiChickenOven className={[styles.icon]} size={iconSize} />
+          ) : temp === 0 ? (
+            <GiFrozenOrb className={[styles.icon]} size={iconSize} />
+          ) : (
+            <GiChicken className={[styles.icon]} size={iconSize} />
+          )}
+
+        </Card.Title>
+        
+        <Card.Title className={[styles.temperature]}>{temp}°C</Card.Title>
+
+        <Form.Range min="0" max="100" value={temp} onChange={onChange} className={styles[tempStyle]}/>
+      </Card.Body>
+    </Card>
+  );
 }
