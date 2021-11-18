0a52af9a4c0148b9966cdc7e950ac3ca68fd6240
Joao Marcelo Alves <joao.alves@liferay.com>
2021-11-18 00:54:40 -0300

    Small Correction

--- src/components/Temperature/index.jsx
+++ src/components/Temperature/index.jsx
@@ -14,7 +14,7 @@ const iconSize = 30;
 export default function Temperature({ temp, manageTemp }) {
   const picture = temp >= 45
     ? "https://oimparcial.com.br/app/uploads/2019/11/como-temperar-frango-para-vender-assado-dicas-passo-a-passo.jpeg"
-    : temp === 0
+    : temp <= 5
       ? "https://thumbs.dreamstime.com/b/chicken-glasses-scarf-isolated-white-background-233547715.jpg"
       : "https://opresenterural.com.br/wp-content/uploads/2021/06/VACCINAR-FOTO-GALINHA-POEDEIRA-VACCINAR-DIVULGA%C3%87%C3%83O-2021-e1623170813779.jpg"
 
@@ -26,7 +26,7 @@ export default function Temperature({ temp, manageTemp }) {
           Temperature
           <label className={[styles.temperature]}>{temp}°C</label>
         </Card.Title>
-        <Card.Text>Temperature inside the Aviary</Card.Text>
+        <Card.Text>Temperature kept inside the Aviary</Card.Text>
         
 
         <Form.Range
